4d814e4c9a8b9e3b3df3f15200e83c168b979bed
Puspanjali14 <neupanepuspanjali@gmail.com>
2025-04-27 12:18:48 +0545

    completing

---
+++ lib/data.ts
@@ -0,0 +1,68 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+ 
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+
+export const projectsData = [
+  {
+    title: "Resume Screening",
+    description:
+      "My final year project where I created beautiful looking website..",
+    tags: ["Html","Css","Javascript", "Flask", "Mysql"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Git",
+  "Tailwind",
+  "Github",
+  "ContextApi",
+  ,
+] as const;
